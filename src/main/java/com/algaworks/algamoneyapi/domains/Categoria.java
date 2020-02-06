a2f3509fbfb826e869eb0e3d546f671af042e7c5
Felipe Guilherme <felipeguilherme644@gmail.com>
2020-02-06 11:30:48 -0300

    Criando Projeto

---
+++ src/main/java/com/algaworks/algamoneyapi/domains/Categoria.java
@@ -0,0 +1,27 @@
+package com.algaworks.algamoneyapi.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String nome;
+
+}
---
+++ src/main/java/com/algaworks/algamoneyapi/domains/TipoLancamento.java
@@ -0,0 +1,28 @@
+package com.algaworks.algamoneyapi.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sun.util.resources.ga.LocaleNames_ga;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class TipoLancamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private Integer receita;
+    private Integer despeasa;
+
+}
