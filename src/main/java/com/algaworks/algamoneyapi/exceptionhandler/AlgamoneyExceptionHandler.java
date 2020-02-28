a18eb9bdeab7121fecbead83a188927afd353962
Felipe Guilherme <felipe.ramos@basis.com.br>
2020-02-28 18:14:23 -0300

    Tratamento de exceção

--- src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,13 +7,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
--- src/main/java/com/algaworks/algamoneyapi/resources/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import com.algaworks.algamoneyapi.services.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,7 +22,7 @@ public class CategoriaResource {
     }
 
     @PostMapping
-    public void save(@RequestBody CategoriaDTO categoriaDTO){
+    public void save(@Valid @RequestBody CategoriaDTO categoriaDTO){
 
         categoriaService.save(categoriaDTO);
     }
