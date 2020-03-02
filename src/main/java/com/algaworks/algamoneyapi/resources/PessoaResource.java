228d13b73121493f504c04ddecb9a7abe7b81ba2
Felipe Guilherme <felipeguilherme644@gmail.com>
2020-03-01 21:41:36 -0300

    Alteraçẽo

--- src/main/java/com/algaworks/algamoneyapi/resources/PessoaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resources/PessoaResource.java
@@ -1,10 +1,30 @@
 package com.algaworks.algamoneyapi.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.algaworks.algamoneyapi.services.CategoriaService;
+import com.algaworks.algamoneyapi.services.dto.CategoriaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public List<CategoriaDTO> listarTodasCategorias() {
+
+        return categoriaService.listarTodasCategorias();
+    }
+
+    @PostMapping
+    public void save(@Valid @RequestBody CategoriaDTO categoriaDTO){
+
+        categoriaService.save(categoriaDTO);
+    }
 
 }
